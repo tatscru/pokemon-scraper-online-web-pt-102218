1e3929e099bde6918d9df431a2f375d19dbfaf26
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 18:49:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,11 +12,11 @@ class Pokemon
   end 
   
   def self.find (id_number, db)
-    pokemon_find = db.execute("SELECT * FROM pokemon WHERE id =?", id_number).first
-    Pokemon.new(
-      id: pokemon_find[0],
-      name: pokemon_find[1],
-      type: pokemon_find[2],
-      db: db)
+    # pokemon_find = db.execute("SELECT * FROM pokemon WHERE id =?", id_number).first
+    # Pokemon.new(
+    #   id: pokemon_find[0],
+    #   name: pokemon_find[1],
+    #   type: pokemon_find[2],
+    #   db: db)
   end 
 end
