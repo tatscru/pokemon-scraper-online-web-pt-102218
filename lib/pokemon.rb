3c90f0a275ed2e76fc78464118b2ceb9e628a2e7
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 18:59:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
   end 
   
   def self.find (id, db)
-       db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+       db.execute("SELECT * FROM pokemon WHERE id = ?").flatten
    
   end 
 end
