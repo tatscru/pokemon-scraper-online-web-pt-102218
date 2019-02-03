4497c972fb7b603d0864f997828c98ad27b2b7e2
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 18:50:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,7 +24,6 @@ class Pokemon
     new_pokemon.id = pokemon[0][0]
     new_pokemon.name = pokemon[0][1]
     new_pokemon.type = pokemon[0][2]
-    new_pokemon.hp = pokemon[0][3]
     return new_pokemon
   end 
 end
