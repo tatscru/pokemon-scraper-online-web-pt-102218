1a08585a11f31cbe044a7a10f6f7b17f1057f2f4
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 19:38:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,6 +7,13 @@ class Pokemon
   #   @@all << self 
   # end 
   
+   def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
   def self.save (name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end 
