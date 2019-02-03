643e75bf6ebc0bc7ed3ff6abad7c9ee1be9608e2
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 19:43:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,10 +8,10 @@ class Pokemon
   # end 
   
   def initialize(id, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+    @@id = id
+    @@name = name
+    @@type = type
+    @@db = db
   end
   
   def self.save (name, type, db)
