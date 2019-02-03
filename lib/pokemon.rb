2e60c3576a4b6c4b83bff2a5d85d47f8c115861e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 19:46:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,6 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
   
-  # def initialize(args)
-  #   @id = id
-  #   @name = name
-  #   @type = type
-  #   @db = db
-  # end
-  
    def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
@@ -19,18 +12,9 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end 
   
-  # def self.find(id, db)
-  #   pokemon_fing = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
-  #   Pokemon.new(
-  #     pokemon_find id: [0], 
-  #     name: [1], 
-  #     type: [2], 
-  #     db: db)
-  # end
-  
   def self.find(id, db)
-    info = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-    Pokemon.new(id: info[0], name: info[1], type: info[2], db: db)
+    db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    Pokemon.new(id: [0], name: info[1], type: info[2], db: db)
   end
 
 end
