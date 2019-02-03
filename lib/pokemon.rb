8f676f1c303ba172ac3ab3c2e0f824f9466b0c9d
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 19:17:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,16 +11,21 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end 
   
-  def self.find (id, db)
-      # db.execute("SELECT * FROM pokemon WHERE id =?", id).flatten
-      # Pokemon.new {
-      #   id: [0],
-      #   name: [1],
-      #   type: [2]
-      # }
+  # def self.find (id, db)
+  #     # db.execute("SELECT * FROM pokemon WHERE id =?", id).flatten
+  #     # Pokemon.new {
+  #     #   id: [0],
+  #     #   name: [1],
+  #     #   type: [2]
+  #     # }
    
-    pokemon_find = db.execute("SELECT * FROM pokemon WHERE id=?",id).flatten
+  #   pokemon_find = db.execute("SELECT * FROM pokemon WHERE id=?",id).flatten
     
-    Pokemon.new(id: pokemon_find[0], name: pokemon_find[1], type: pokemon_find[2], db: db )
-  end 
+  #   Pokemon.new(id: pokemon_find[0], name: pokemon_find[1], type: pokemon_find[2], db: db )
+  # end 
+  
+    def self.find(id_number, db)
+    pokemon_find = db.execute("SELECT * FROM pokemon WHERE id=?", id_number).first
+    Pokemon.new(id: pokemon_find[0], name: pokemon_find[1], type: pokemon_find[2], hp: pokemon_find[3], db: db )
+  end
 end
