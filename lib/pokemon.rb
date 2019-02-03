d89f0b99035b44eb4898b2daf718f3800d0329a3
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 19:20:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,6 +18,9 @@ class Pokemon
   #     #   name: [1],
   #     #   type: [2]
   #     # }
-
+  def self.find(id_number, db)
+    pokemon_find = db.execute("SELECT * FROM pokemon WHERE id=?", id_number).first
+    Pokemon.new(id: pokemon_find[0], name: pokemon_find[1], type: pokemon_find[2], db: db )
+  end
 
 end
