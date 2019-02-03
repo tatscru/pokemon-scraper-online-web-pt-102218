227b84ec00023bb9eebd819ff844285519210edb
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 19:18:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -31,9 +31,9 @@ class Pokemon
   def 
    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
     new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
+    new_pokemon.id = pokemon[0][0],
+    new_pokemon.name = pokemon[0][1],
+    new_pokemon.type = pokemon[0][2],
     new_pokemon.hp = pokemon[0][3]
     return new_pokemon
   end 
