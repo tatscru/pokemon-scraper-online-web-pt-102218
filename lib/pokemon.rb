ce910559f5a93afabf4548902f974dfd52f3df33
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 18:15:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   
   def initialize (args)
-    # @id = id 
-    # @name = name 
-    # @type = type 
-    # @db = db
+  
   end 
   
   def self.save (name, type, db)
